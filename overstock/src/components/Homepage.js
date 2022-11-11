40520b45e111c623ced70d3a9d1a0d6a8121b72d
Swgatsahu99 <swagat.sahu.2001@gmail.com>
2022-11-11 11:53:01 +0530

    homepage completed

---
+++ overstock/src/components/Homepage.js
@@ -0,0 +1,195 @@
+function Homepage(){
+    return (
+        <>
+       <header>
+  <div className="black-sale">
+    <a href="#" className="black-friday">
+      <div className="home-page">
+        <img src="https://ak1.ostkcdn.com/img/mxc/10312022-A1-HP-INTL-1400x550_DSK-INTL.jpg" alt />
+      </div>
+      <div className="home-txt">
+        <img src="https://ak1.ostkcdn.com/img/mxc/10312022-A1-HP-INTL-1400x550_DSK-INTL.svg" alt />
+      </div>
+    </a>
+    <h1>Limited-Time Deals</h1>
+  </div>
+  {/* deck-hell */}
+  <div className="deck-hall">
+    <a href="#" className="black-friday">
+      <div className="home-page">
+        <img src="https://ak1.ostkcdn.com/img/mxc/10312022-SB-HolidayDecor-1400x550_Promo2v3.jpg?imwidth=1920" alt />
+      </div>
+      <div className="deck-hall-img">
+        <img src="https://ak1.ostkcdn.com/img/mxc/10312022-SB-HolidayDecor-1400x550_Promo1.svg?imwidth=1920" alt />
+      </div>
+    </a>
+    <h1>Fresh Finds In Each Category</h1>
+  </div>
+  {/* fresh Category */}
+  <div className="fresh-category">
+    <div className="fresh-content">
+      <a href="#">
+        <img src="https://ak1.ostkcdn.com/img/mxc/11192021-CatSilo-Furniture.png?imwidth=1920" alt="#" />
+      </a>
+      <p>Furniture</p>
+    </div>
+    <div className="fresh-content">
+      <a href="#">
+        <img src="https://ak1.ostkcdn.com/img/mxc/11152021-CatSilo-AreaRugs.png?imwidth=1920" alt="#" />
+      </a>
+      <p>Area Rugs</p>
+    </div>
+    <div className="fresh-content">
+      <a href="#">
+        <img src="https://ak1.ostkcdn.com/img/mxc/11152021-CatSilo-BedBath.png?imwidth=1920" alt="#" />
+      </a>
+      <p>Bedding</p>
+    </div>
+    <div className="fresh-content">
+      <a href="#">
+        <img src="https://ak1.ostkcdn.com/img/mxc/11152021-CatSilo-Decor.png?imwidth=1920" alt="#" />
+      </a>
+      <p>Home Decore</p>
+    </div>
+    <div className="fresh-content">
+      <a href="#">
+        <img src="https://ak1.ostkcdn.com/img/mxc/CatSilo-Window-040721.jpg?imwidth=1920" alt="#" />
+      </a>
+      <p>Window Treatements</p>
+    </div>
+    <div className="fresh-content">
+      <a href="#">
+        <img src="https://ak1.ostkcdn.com/img/mxc/11152021-CatSilo-Kitchen.png?imwidth=1920" alt="#" />
+      </a>
+      <p>Kitchen</p>
+    </div>
+    <div className="fresh-content">
+      <a href="#">
+        <img src="https://ak1.ostkcdn.com/img/mxc/11192021-CatSilo-Outdoor.png?imwidth=1920" alt="#" />
+      </a>
+      <p>Outdoor</p>
+    </div>
+    <div className="fresh-content">
+      <a href="#">
+        <img src="https://ak1.ostkcdn.com/img/mxc/01112021-CatSilo-HomeImp.jpg?imwidth=1920" alt="#" />
+      </a>
+      <p>Home Improvement</p>
+    </div>
+    <div className="fresh-content">
+      <a href="#">
+        <img src="https://ak1.ostkcdn.com/img/mxc/11152021-CatSilo-Storage.png?imwidth=1920" alt="#" />
+      </a>
+      <p>Storage</p>
+    </div>
+    <div className="fresh-content">
+      <a href="#">
+        <img src="https://ak1.ostkcdn.com/img/mxc/01112021-CatSilo-Mattresses.jpg?imwidth=1920" alt="#" />
+      </a>
+      <p>Mattresses</p>
+    </div>
+    <div className="fresh-content">
+      <a href="#">
+        <img src="https://ak1.ostkcdn.com/img/mxc/11152021-CatSilo-Lighting.png?imwidth=1920" alt="#" />
+      </a>
+      <p>Lighting</p>
+    </div>
+    <div className="fresh-content">
+      <a href="#">
+        <img src="https://ak1.ostkcdn.com/img/mxc/CatSilo-AllDeals-040721.jpg?imwidth=1920" alt="#" />
+      </a>
+      <p>Shop All Deal</p>
+    </div>
+  </div>
+  <h1 style={{textAlign: 'center'}}>Seasonal Updates to Warm up Your Winter</h1>
+  {/* seasonal update */}
+  <div className="seasonal-update">
+    <div className="seasonal-update-content">
+      <a href="#">
+        <img src="https://ak1.ostkcdn.com/img/mxc/10272022-MOPS-HP-Frame1.jpg?imwidth=1920" alt="#" />
+        <h2>home gifts</h2>
+        <p>Gift-ready home goods for everyone on your list</p>
+      </a>
+    </div>
+    <div className="seasonal-update-content">
+      <a href="#">
+        <img src="https://ak1.ostkcdn.com/img/mxc/10272022-MOPS-HP-Frame2.jpg?imwidth=1920" alt="#" />
+        <h2>Kitchen applications</h2>
+        <p>Small but mighty tools that make food prep a breeze</p>
+      </a>
+    </div>
+    <div className="seasonal-update-content">
+      <a href="#">
+        <img src="https://ak1.ostkcdn.com/img/mxc/10272022-MOPS-HP-Frame3.jpg?imwidth=1920" alt="#" />
+        <h2>Christmas trees</h2>
+        <p>The perfect place to put those perfect gifts.</p>
+      </a>
+    </div>
+    <div className="seasonal-update-content">
+      <a href="#">
+        <img src="https://ak1.ostkcdn.com/img/mxc/10272022-MOPS-HP-Frame4.jpg?imwidth=1920" alt="#" />
+        <h2>holiday decore</h2>
+        <p>Deck your halls with the season’s festive finest.</p></a>
+    </div>
+  </div>
+  {/* guest room */}
+  <div className="deck-hall">
+    <a href="#" className="black-friday">
+      <div className="home-page">
+        <img src="https://ak1.ostkcdn.com/img/mxc/10312022-SB-GuestReadyBedroom-1400x550_Promo2v2.jpg?imwidth=1920" alt />
+      </div>
+      <div className="deck-hall-img">
+        <img src="https://ak1.ostkcdn.com/img/mxc/10312022-SB-GuestReadyBedroom-1400x550_Promo2.svg?imwidth=1920" alt />
+      </div>
+    </a>
+    <h1>Overstock Featured Brands</h1>
+  </div>
+  {/* Overstock Featured Brands */}
+  <div className="overstock-feature">
+    <div className="seasonal-update-content">
+      <a href="#">
+        <img src="https://ak1.ostkcdn.com/img/mxc/HP_C_Mod_21238648.jpg?imwidth=1920" alt="#" />
+        <h4>Extra 15% off</h4>
+        <p>Select Bean Bags &amp; More by Christopher Knight*</p>
+      </a>
+    </div>
+    <div className="seasonal-update-content">
+      <a href="#">
+        <img src="https://ak1.ostkcdn.com/img/mxc/HP_C_Mod_39766562.jpg?imwidth=1920" alt="#" />
+        <h4>Extra 15% off</h4>
+        <p>Select Lightining by JONATHAN Y*</p>
+      </a>
+    </div>
+    <div className="seasonal-update-content">
+      <a href="#">
+        <img src="https://ak1.ostkcdn.com/img/mxc/HP_C_Mod_37373953.jpg?imwidth=1920" alt="#" />
+        <h4>Extra 15% off </h4>
+        <p>Select Furniture by Angelo Home*</p>
+      </a>
+    </div>
+    <div className="seasonal-update-content">
+      <a href="#">
+        <img src="https://ak1.ostkcdn.com/img/mxc/HP_C_Mod_37777461.jpg?imwidth=1920" alt="#" />
+        <h4>Extra 15% off </h4>
+        <p>Select Furniture by Angelo Home*</p>
+      </a>
+    </div>
+  </div>
+  {/* overstock-style-life */}
+  <div className="deck-hall">
+    <a href="#" className="black-friday">
+      <div className="home-page">
+        <img src="https://ak1.ostkcdn.com/img/mxc/030822-RIUpdate-Dsk.jpg?imwidth=1920" alt="#" />
+      </div>
+      <div className="lifecontent-2">
+        <img src="https://ak1.ostkcdn.com/img/mxc/030822-RIUpdate-Dsk.svg?imwidth=1920" alt="#" />
+      </div>
+    </a>
+    <h1>We've Got Your Style</h1>
+  </div>
+</header>
+
+        </>
+    );
+}
+
+export default Homepage;
